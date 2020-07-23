0f11f5e49231b9b7100af5ad853365ad944380b9
Alyssa-rivera <alyssariveraw@gmail.com>
2020-07-23 18:51:56 +0000

    updated w/ meiqin's work!

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ from flask import request
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
 # name of database
 # app.config['MONGO_DBNAME'] = 'database-name'
 
@@ -24,23 +18,33 @@ events = [
 # mongo = PyMongo(app)
 
 # -- Routes section --
+
+# HOMEPAGE
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+
 # INDEX
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return render_template('index.html', events = events)
+    return "Hello World"
+
+# def index():
+#     return render_template('index.html', events = events)
 
 
 # CONNECT TO DB, ADD DATA
 
-@app.route('/add')
 
-def add():
+# @app.route('/add')
+
+# def add():
     # connect to the database
 
     # insert new data
 
-    # return a message to the user
-    return ""
+    # return a message to the use
