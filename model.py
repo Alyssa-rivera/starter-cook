dcf3a3b059cba99f4b60a5d60ba248d1c49c9ce2
mgao1307 <minervagao2002@gmail.com>
2020-07-30 16:28:15 +0000

    fix functionality

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
 import requests
 import random
 
-# def getRecipe(query, key):
-#     recipe_query = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
-#     response = requests.get(recipe_query).json()
-#     return response['data'][0]['images']['fixed_height']['url']
 
 
 # def get(query,key):
